e66cd96ec6a7c969e7ea377326fcdd4b00cf1e07
deejasq <damo@deejasq.com>
2026-05-18 17:00:15 +0200

    Initial upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'lbf26-v1';
+const SHELL = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(caches.match('./index.html').then(r => r || fetch(e.request)));
+    return;
+  }
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
